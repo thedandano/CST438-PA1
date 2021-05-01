596b57def1378abeeb3345ff2456b874fa296f57
Dan Sedano <dandano2006@gmail.com>
2021-04-30 19:26:39 -0700

    first commit

---
+++ CST438-PA1/src/test/java/cst438/Cst438Pa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cst438;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cst438Pa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
